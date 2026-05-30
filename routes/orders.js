ec9caf643d0c7e04323a9302ad620d557b0d5f40
Sneha-datla <datla.sneha1@gmail.com>
2026-05-30 16:34:02 +0530

    changes made

--- routes/orders.js
+++ routes/orders.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const pool = require("../db");
-const auth = require("./auth");
 
 // CREATE ORDER
 router.post("/place_order", auth, async (req, res) => {
